349668e31150277ce7aaf1ce8183ae9f2ad2e705
Felipe Beser <felipebeserx@gmail.com>
2022-09-24 23:18:22 -0300

    cs

--- streamCollectionsThread/src/network3/Client.java
+++ streamCollectionsThread/src/network3/Client.java
@@ -1,24 +1,52 @@
 package network3;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
 public class Client {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
+		JFrame frame = new JFrame("Chat");
+		frame.setSize(400, 200);
+
+		JTextArea caixaTexto = new JTextArea();
+		JTextField mensagem = new JTextField();
+		
+		frame.add(mensagem,BorderLayout.PAGE_END);
+		frame.add(caixaTexto,BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		
 		Socket client = new Socket("localhost", 10000);
-		System.out.println("Cliente Conectado.");
-		Scanner sc = new Scanner(System.in);
+		caixaTexto.append("Cliente Conectado.");
+//		Scanner sc = new Scanner(System.in);
 		PrintStream ps = new PrintStream(client.getOutputStream());
-		while(sc.hasNextLine() || sc.nextLine() == "sair" ) {
-			ps.println(sc.nextLine());
-		}
+//		while(sc.hasNextLine() || sc.nextLine() == "sair" ) {
+//			ps.println(sc.nextLine());
+//		}
+		mensagem.addActionListener(
+		         new ActionListener() {
+		            public void actionPerformed( ActionEvent e ){
+		            	ps.println(mensagem);
+		            }
+		         }
+		      );
 		System.out.println("Saindo..");
 		ps.close();
-		sc.close();
+//		sc.close();
 		client.close();
 	}
 
--- streamCollectionsThread/src/network3/Server.java
+++ streamCollectionsThread/src/network3/Server.java
@@ -1,25 +1,61 @@
 package network3;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+
 public class Server {
 
 	public static void main(String[] args) throws IOException {
+		JFrame frame = new JFrame("Chat");
+		frame.setSize(400, 200);
+
+		JTextArea caixaTexto = new JTextArea();
+		JTextField mensagem = new JTextField();
+		mensagem.setEnabled(false);
+		frame.add(mensagem,BorderLayout.PAGE_END);
+		frame.add(caixaTexto,BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		
 		ServerSocket server = new ServerSocket(10000);
-		System.out.println("Aguardando conexão..");
+		caixaTexto.append("Aguardando conexão..");
 		Socket client = server.accept();
-		System.out.println("Conexão estabelecida!");
-		Scanner sc = new Scanner(client.getInputStream());
-		while(sc.hasNextLine()|| sc.nextLine().equals("sair")) {
-			System.out.println("Cliente: " + sc.nextLine());
-		}
+		caixaTexto.append("Conexão estabelecida!");
+		
+		mensagem.setEnabled(true);
+		mensagem.addActionListener(
+		         new ActionListener() {
+		            public void actionPerformed( ActionEvent e ){
+		            	Scanner sc = new Scanner(mensagem.getSelectedText());
+		        		try {
+							PrintStream ps = new PrintStream(client.getOutputStream());
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		        		while(sc.hasNextLine() || sc.nextLine().equals("sair")) {
+		        			System.out.println("Cliente: " + sc.nextLine());
+		        		}
+		        		sc.close();
+		            }
+		         }
+		      );
+		
 		System.out.println("Saindo..");
-		sc.close();
 		client.close();
 		server.close();
 		}
-
 }
